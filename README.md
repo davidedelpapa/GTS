b0db0266ffe7e0bed2edb07cd2d4ba81b030c080
oldmammuth <davidedelpapa@gmail.com>
2017-06-21 23:25:00 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GTS
-PHP Tile Server with User (and keys) management
+PHP Tile Server with User (and keys) Management
+
+**Important Notes**:
+
+- The system uses *.htaccess* and *mod-rewrite*
+- Configuration is handled inside the gts.php file
+- testing-tiles kindly provided by [SentinelMap.eu](https://sentinelmap.eu): to test it, use the test.html provided (after having set up the DB and gts.php)
+
+(more info coming soon...)
---
+++ gts.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ * Debug Code
+ */
+/*
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+*/
+$tile_tree_base = './testing-tiles/';
+
+/* ----------------------------------------------------- */
+/*                      SERVER SETUP                     */
+/* ----------------------------------------------------- */
+
+$db_connection = new mysqli(
+    'localhost',// host
+    'gts', 		// user
+    '********',	// password
+    '',			// database
+    '3306'      // port
+);
+if ($db_connection->connect_error) {
+    error_log('MySql Connection failed: ' . $db_connection->connect_error);
+    http_response_code(500);
+    die(1);
+}
+
+/* 
+ * Do not serve the tile without a proper Api Key
+ * In case of unauthorized access, returns a HTTP 403 error code.
+ */
+if(isset( $_REQUEST['key'])){
+    $key = $db_connection->real_escape_string($_REQUEST['key']);
+} else {
+    http_response_code(403);
+    die(1);
+}
+
+/* 
+ * Allowing serving tiles on the necessary conditions only:
+ * - User must exist, and the key correspond to it.
+ * - User must not have reached the max tile cap
+ */
+
+// First condition: wrong key? No user connected to it? -> Error 500
+$result = $db_connection->query("SELECT management.Users.userid, management.Users.tilecount, management.Users.tilemax FROM management.Users INNER JOIN gts.pkeys ON management.Users.userid = gts.pkeys.userid WHERE gts.pkeys.pkey = '" . $key . "';");
+if (!$result) {
+    http_response_code(500);
+    die(1);
+} else {
+    $r_userdata = $result->fetch_row();
+    $result->free();
+    $userid =  $r_userdata[0];
+    $curr_tile_count = $r_userdata[1];
+    $max_tiles = $r_userdata[2];
+}
+
+// second condition: Reached the tile cap? -> Error 403
+if (!$userid || ($curr_tile_count + 1)>=$max_tiles){
+    http_response_code(403);
+    die(1);
+}
+
+/* ----------------------------------------------------- */
+/*                    SERVER ROUTINES                    */
+/* ----------------------------------------------------- */
+    
+/* Prepare Response */
+$file = $tile_tree_base . $_REQUEST['z'] . '/' . $_REQUEST['x'] . '/' . $_REQUEST['y'];
+
+/* Check file existence */
+if(!file_exists($file)){
+    $db_connection->close();
+    http_response_code(404);
+    die(1);
+}
+
+/* Update tile counters (per-user, and per-key counters) */
+$db_connection->query("UPDATE management.Users INNER JOIN gts.pkeys ON (management.Users.userid = gts.pkeys.userid) SET management.Users.tilecount = management.Users.tilecount + 1, gts.pkeys.tilecount = gts.pkeys.tilecount + 1 WHERE gts.pkeys.pkey = '" .$key . "';");
+
+/* Actual Response */
+$db_connection->close();
+
+header('Content-Type: image/jpeg');        
+readfile($file);
+?>
